1875aa2beb541aa28b0a605bec7e085a1bb3cd1f
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-03-23 01:08:00 +0530

    Added a Nested HTML Structure using React

--- 1.Inception/app.js
+++ 1.Inception/app.js
@@ -1,4 +1,25 @@
-const heading = React.createElement("h1", { id: "Heading" , xyz: "abc"}, "Hello World From React!");
+
+/**
+ * 
+ * <div id="parent">
+ * 	<div id="child">
+ * 		<h1>I'm h1 Tag</h1> 
+ * 	</div>
+ * </div>
+ */
+
+const parent = React.createElement(
+	"div",
+	{ id: "parent" },
+	React.createElement("div", { id: "child" }, React.createElement("h1", {}, "I'm an h1 Tag"))
+);
+
+
+
+const heading = React.createElement("h1", { id: "Heading", xyz: "abc" }, "Hello World From React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+console.log(parent);
+
+// root.render(heading);
+root.render(parent);
