4a7c94de61c7316373301dc3a77cfde24f29fbca
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-03-23 01:20:42 +0530

    Added a Parent->Child->Siblings  using React

--- 1.Inception/app.js
+++ 1.Inception/app.js
@@ -1,24 +1,29 @@
-
 /**
- * 
+ *
  * <div id="parent">
  * 	<div id="child">
- * 		<h1>I'm h1 Tag</h1> 
+ * 		<h1>I'm h1 Tag</h1>
+ * 		<h2>I'm a h2 Tag</h2>
  * 	</div>
  * </div>
- * 
+ *
  * ReactElement -> Is a Object => While It into the DOM it Becomes HTML
  */
 
 const parent = React.createElement(
-	"div",
-	{ id: "parent" }, // Attribute
-	React.createElement("div", { id: "child" }, React.createElement("h1", {}, "I'm an h1 Tag"))
+  "div",
+  { id: "parent" }, // Attribute
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 Tag"),
+    React.createElement("h2", {}, "I'm an h2 Tag"),
+  ])
 );
 
-
-
-const heading = React.createElement("h1", { id: "Heading", xyz: "abc" }, "Hello World From React!");
+const heading = React.createElement(
+  "h1",
+  { id: "Heading", xyz: "abc" },
+  "Hello World From React!"
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 console.log(parent);
