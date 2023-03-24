83aead16fd89c26735484da98e0f67f82a49acd4
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-03-24 13:51:26 +0530

    Added More Complex Nexted Structure

--- 1.Inception/app.js
+++ 1.Inception/app.js
@@ -11,18 +11,25 @@
  */
 
 const parent = React.createElement(
-  "div",
-  { id: "parent" }, // Attribute
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 Tag"),
-    React.createElement("h2", {}, "I'm an h2 Tag"),
-  ])
+	"div",
+	{ id: "parent" }, // Attribute
+	[React.createElement("div", { id: "child1" }, [
+		React.createElement("h1", {}, "I'm an h1 Tag"),
+		React.createElement("h2", {}, "I'm an h2 Tag"),
+	]),
+	React.createElement("div", { id: "child2" }, [
+		React.createElement("h1", {}, "I'm an h1 Tag"),
+		React.createElement("h2", {}, "I'm an h2 Tag"),
+	])]
+
 );
 
+// JSX -> To make our work easy in writing html tags we use it.
+
 const heading = React.createElement(
-  "h1",
-  { id: "Heading", xyz: "abc" },
-  "Hello World From React!"
+	"h1",
+	{ id: "Heading", xyz: "abc" },
+	"Hello World From React!"
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
