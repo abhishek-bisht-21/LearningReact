35d8e723967d7c104021646f46b7619b49ca4e7c
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-04-08 13:31:14 +0530

    General Info around JSX,parcel and Babel

--- 3.LayingTheFoundation/app.js
+++ 3.LayingTheFoundation/app.js
@@ -17,9 +17,12 @@ JSX is a HTML like Syntax.
 JSX code is transpiled before it reaches the javacript engine. 
 Transpiled means that it is converted to the code which the browser and react can understand.
 Parcel is responsible for transpiling this code. Parcel further delegates this transpilation job
-to a Package called as "Babel"
+to a Package called as "Babel".
 */
 
-const jsxHeading = <h1 id="heading"> Namaste React using JSX ⚡</h1>
+// JSX => React.createElement => ReactElement-JS Object => HTMLElement(render)
+const jsxHeading = <h1 className="head"> Namaste React using JSX ⚡</h1>
+root.render(jsxHeading);
+
 console.log(jsxHeading);
 
